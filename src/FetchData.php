5420fae4293a63b27822d2818baa93b102bb8ff6
Enjoys <deadly.info@gmail.com>
2022-06-21 14:14:12 +0300

    fix cacher

--- src/FetchData.php
+++ src/FetchData.php
@@ -8,29 +8,21 @@ namespace EnjoysCMS\WidgetYaMetrika;
 
 use AXP\YaMetrika\Client;
 use AXP\YaMetrika\Exception\FormatException;
+use Enjoys\SimpleCache\Cacher\FileCache;
 use EnjoysCMS\Core\Entities\Widget;
 use Psr\SimpleCache\CacheInterface;
 use Psr\SimpleCache\InvalidArgumentException;
 
 final class FetchData
 {
-    private \AXP\YaMetrika\YaMetrika $metrika;
     private ?CacheInterface $cacher = null;
+    private $client = null;
 
     public function __construct(private Widget $widget)
     {
-        $client = new Client(
-            $_ENV['YA_METRIKA_TOKEN'] ?? throw new \InvalidArgumentException(
-                'Set in .env `YA_METRIKA_TOKEN`. See <a href="https://github.com/axp-dev/ya-metrika#Получение-токена">here</a>'
-            ),
-            $this->getCounterId()
-            ?? $_ENV['YA_METRIKA_COUNTER_ID']
-            ?? throw new \InvalidArgumentException(
-                'Set counter id of yandex metrika in setting widget or in .env parameter `YA_METRIKA_COUNTER_ID`'
-            )
-        );
-        $this->metrika = new \AXP\YaMetrika\YaMetrika($client);
-        $this->cacher = include __DIR__ . '/Cacher.php';
+        $this->cacher = new FileCache([
+            'path' => $_ENV['TEMP_DIR'] . '/cache/yametrika-widgets'
+        ]);
     }
 
     private function getCounterId()
@@ -53,6 +45,24 @@ final class FetchData
         return md5(json_encode([$prefix, $this->widget->getOptions(), $this->widget->getId()]));
     }
 
+    private function getClient(): \AXP\YaMetrika\YaMetrika
+    {
+        if ($this->client === null){
+            $client = new Client(
+                $_ENV['YA_METRIKA_TOKEN'] ?? throw new \InvalidArgumentException(
+                    'Set in .env `YA_METRIKA_TOKEN`. See <a href="https://github.com/axp-dev/ya-metrika#Получение-токена">here</a>'
+                ),
+                $this->getCounterId()
+                ?? $_ENV['YA_METRIKA_COUNTER_ID']
+                ?? throw new \InvalidArgumentException(
+                    'Set counter id of yandex metrika in setting widget or in .env parameter `YA_METRIKA_COUNTER_ID`'
+                )
+            );
+            $this->client = new \AXP\YaMetrika\YaMetrika($client);
+        }
+        return $this->client;
+    }
+
     /**
      * @throws FormatException
      * @throws InvalidArgumentException
@@ -62,11 +72,11 @@ final class FetchData
         $cacheId = $this->getCacheId('visitors');
 
         if (null === $data = $this->cacher?->get($cacheId)) {
-            $data = $this->metrika->getVisitorsForPeriod(
+            $data = $this->getClient()->getVisitorsForPeriod(
                 $startDate ?? (new \DateTime())->modify('-30 days'),
                 $endDate ?? new \DateTime()
             )->formatData();
-            $this->cacher->set($cacheId, $data, (int)($this->widget->getOptions()['cache'] ?? 0));
+            $this->cacher->set($cacheId, $data, (int)($this->widget->getOptions()['cache']['value'] ?? 0));
         }
 
         return $data;
@@ -81,12 +91,12 @@ final class FetchData
         $cacheId = $this->getCacheId('browsers');
 
         if (null === $data = $this->cacher?->get($cacheId)) {
-            $data = $this->metrika->getBrowsersForPeriod(
+            $data = $this->getClient()->getBrowsersForPeriod(
                 $startDate ?? (new \DateTime())->modify('-30 days'),
                 $endDate ?? new \DateTime(),
                 $limit
             )->formatData();
-            $this->cacher->set($cacheId, $data, (int)($this->widget->getOptions()['cache'] ?? 0));
+            $this->cacher->set($cacheId, $data, (int)($this->widget->getOptions()['value'] ?? 0));
         }
 
         return $data;
