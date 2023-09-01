7946dbe1903695df0bb440968d4897d8db8ef384
enjoys <deadly.info@gmail.com>
2023-09-01 20:40:47 +0300

    update post-install-cmd script

---
+++ scripts/AssetsInstallCommand.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace EnjoysCMS\WidgetYaMetrika\Composer\Scripts;
+
+use EnjoysCMS\Core\Console\Command\AbstractAssetsInstallCommand;
+
+class AssetsInstallCommand extends AbstractAssetsInstallCommand
+{
+    protected string $cwd = __DIR__ . '/..';
+}
